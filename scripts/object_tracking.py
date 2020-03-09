4b06183bac4825e38a6db9d4ff48bffae5f68b86
ShotaAk <s.aoki@rt-net.jp>
2020-03-09 15:25:42 +0900

    Refactoring: detect_centroid

--- scripts/object_tracking.py
+++ scripts/object_tracking.py
@@ -3,6 +3,7 @@
 
 import rospy, cv2, math
 import numpy as np
+import copy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
@@ -96,28 +97,37 @@ class ObjectTracker():
         if self._object_pixels_default == 0 and self._object_pixels != 0:
             self._object_pixels_default = self._object_pixels
 
-    def _detect_centroid(self, binary):
-        self._object_pixels = 0
-        area_max_num = 0
-        _, contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        point_centroid = (self._captured_image.shape[1], self._captured_image.shape[0])
-        # Find index of maximum area
+    def _extract_biggest_contour(self, binary_img):
+        biggest_contour_index = False
+        biggest_contour_area = 0
+        _, contours, hierarchy = cv2.findContours(binary_img, 
+                cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for i, cnt in enumerate(contours):
             area = cv2.contourArea(cnt)
-            if self._object_pixels < area:
-                self._object_pixels = area
-                area_max_num = i
-        # Define object_pixels_default
-        self._calibrate_object_pixels_default()
-        # Draw countours
-        centroid_img = cv2.drawContours(self._captured_image, contours, area_max_num, (0, 255, 0), 5)
+            if biggest_contour_area < area:
+                biggest_contour_area  = area
+                biggest_contour_index = i
+
+        if biggest_contour_index is False:
+            return False
+        else:
+            return contours[biggest_contour_index]
+
+    def _calculate_centroid_point(self, contour):
+        point = False
         if self._object_is_detected():
-            M = cv2.moments(contours[area_max_num])
+            M = cv2.moments(contour)
             centroid_x = int(M['m10'] / M['m00'])
             centroid_y = int(M['m01'] / M['m00'])
-            point_centroid = (centroid_x, centroid_y)
-            centroid_img = cv2.circle(centroid_img, point_centroid, 15, (255, 0, 0), thickness=-1) 
-        return centroid_img, point_centroid
+            point = (centroid_x, centroid_y)
+
+        return point
+
+    def _draw_contour(self, input_image, contour):
+        return cv2.drawContours(input_image, [contour], 0, (0, 255, 0), 5)
+
+    def _draw_centroid(self, input_image, point_centroid):
+        return cv2.circle(input_image, point_centroid, 15, (255, 0, 0), thickness=-1) 
 
     def _monitor(self, img, pub):
         if img.ndim == 2:
@@ -129,7 +139,7 @@ class ObjectTracker():
 
     # Determine rotation angle from center of gravity position
     def _rot_vel(self):
-        if not self._object_is_detected():
+        if not self._object_is_detected() or self._point_of_centroid is None:
             return 0.0
         wid = self._captured_image.shape[1]/2
         pos_x_rate = (self._point_of_centroid[0] - wid)*1.0/wid
@@ -138,11 +148,25 @@ class ObjectTracker():
         return rot
 
     def image_processing(self):
+        object_image = copy.deepcopy(self._captured_image)
         object_binary_img = self._extract_object_in_binary(self._captured_image)
+
         if not object_binary_img is None:
+            biggest_contour = self._extract_biggest_contour(object_binary_img)
+            if not biggest_contour is False:
+                self._object_pixels = cv2.contourArea(biggest_contour)
+                self._calibrate_object_pixels_default()
+
+                object_image = self._draw_contour(object_image,
+                        biggest_contour)
+
+                point = self._calculate_centroid_point(biggest_contour)
+                if not point is False:
+                    self._point_of_centroid = point
+                    object_image = self._draw_centroid(object_image, point)
+
             self._monitor(object_binary_img, self._pub_binary_image)
-            centroid_img, self._point_of_centroid = self._detect_centroid(object_binary_img)
-            self._monitor(centroid_img, self._pub_pbject_image)
+            self._monitor(object_image, self._pub_pbject_image)
 
     def control(self):
         cmd_vel = Twist()
